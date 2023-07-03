c83b086b64d0cc9f8f1f3a376ffa68a72f947c02
fishjojo <zhangxing.nju@gmail.com>
2023-07-02 23:41:12 -0700

    add pack_tril_vjp

--- pyscf/lib/np_helper/pack_tril_vjp.c
+++ pyscf/lib/np_helper/pack_tril_vjp.c
@@ -49,3 +49,27 @@ void NPdunpack_tril_2d_vjp(int count, int n, double *tril, double *mat, int herm
         }
     }
 }
+
+void NPdpack_tril_vjp(int n, double *tril, double *mat)
+{
+    size_t i, j, ij;
+    for (ij = 0, i = 0; i < n; i++) {
+        for (j = 0; j <= i; j++, ij++) {
+            mat[i*n+j] = tril[ij];
+        }
+    }
+}
+
+void NPdpack_tril_2d_vjp(int count, int n, double *tril, double *mat)
+{
+    #pragma omp parallel
+    {
+        int ic;
+        size_t nn = n * n;
+        size_t n2 = n*(n+1)/2;
+        #pragma omp for schedule (static)
+        for (ic = 0; ic < count; ic++) {
+            NPdpack_tril_vjp(n, tril+n2*ic, mat+nn*ic);
+        }
+    }
+}
